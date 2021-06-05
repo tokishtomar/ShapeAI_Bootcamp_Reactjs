a2c4f9d8e29649a82173629ad330752f647afce2
Tokish Tomar <60000444+tokishtomar@users.noreply.github.com>
2021-06-05 22:33:30 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Note = () => {
+    return (
+        <div className='note'>
+            <h1>JavaScript & ReactJs</h1>
+            <p>This was an amazing bootcamp taken up by Shaurya Sinha. 
+                We covered everything from scratch including JavaScript, ReactJs and HTML.</p>
+        </div>
+    )
+}
+
+export default Note;
